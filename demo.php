bac2d24eb20785aa27a40bfa42f4cf5365db827a
galaxy-zh <1603649280@qq.com>
2022-05-06 16:32:45 +0800

    last version

--- demo.php
+++ demo.php
@@ -1,12 +1,12 @@
 <?php
 require("vendor/autoload.php");
 $conf = [
-    'service' => '',
-    'port' => '',
-    'user' => '',
-    'passord' => '',
+    'service' => 'smtp.qq.com',
+    'port' => '465',
+    'user' => '1603649280@qq.com',
+    'passord' => 'powwbtrnqgjcgdhb',
 ];
-$obj = new phpEmail\phpEmail\SendMail($conf['service'], $conf['port'], $conf['user'], $conf['passord']);
+$obj = new \phpEmail\phpEmail\SendMail($conf['service'], $conf['port'], $conf['user'], $conf['passord']);
 
 //构建邮件
 $mail = array(
